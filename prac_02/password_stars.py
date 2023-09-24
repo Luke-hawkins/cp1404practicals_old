538fe724e6ecbf30e97759cf51b550936779fd1b
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-09-24 17:13:20 +1000

    add Password_stars

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,18 @@
-password = input("Password: ")
-PASSWORD_MIN_LENGTH = 0
-while len(password) <= PASSWORD_MIN_LENGTH:
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
     password = input("Password: ")
-print(len(password) * "*")
+    PASSWORD_MIN_LENGTH = 0
+    while len(password) <= PASSWORD_MIN_LENGTH:
+        password = input("Password: ")
+    return password
+
+
+def print_asterisks(password):
+    print(len(password) * "*")
+
+
+main()
