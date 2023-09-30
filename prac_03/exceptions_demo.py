cf1a34ef654ae275087c49a0c6141b913f9a2698
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-09-30 20:42:34 +1000

    Added questions 1 - 3 to exceptions_demo.py.

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+
+A value error occurs when the used value does not fit within the parameters of the function
+such as finding a square root of a negative number.
+
+2. When will a ZeroDivisionError occur?
+
+when a number is divided by zero
+
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+
+Yes, you could error check to make sure the input for the denominator is not zero
+"""
+
+
+def main():
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+        fraction = numerator / denominator
+        print(fraction)
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+    print("Finished.")
+
+
+main()
