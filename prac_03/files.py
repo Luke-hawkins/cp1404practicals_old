485bc3062abe6cc65d799bcd8d7420e113b480af
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-09-30 22:27:49 +1000

    Added question 3 to files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,13 +1,31 @@
 def main():
-    OUTPUT_FILE = "name.txt"
-    question_1(OUTPUT_FILE)
+    OUTPUT_FILE_FOR_NAME = "name.txt"
+    OUTPUT_FILE_FOR_NUMBERS = "numbers.txt"
+    # question_1(OUTPUT_FILE_FOR_NAME)
+    # question_2(OUTPUT_FILE_FOR_NAME)
+    question_3(OUTPUT_FILE_FOR_NUMBERS)
 
 
-def question_1(OUTPUT_FILE):
+def question_1(OUTPUT_FILE_FOR_NAME):
     name = input("Name: ")
-    out_file = open(OUTPUT_FILE, 'w')
+    out_file = open(OUTPUT_FILE_FOR_NAME, 'w')
     print(name, file=out_file)
     out_file.close()
+
+
+def question_2(OUTPUT_FILE_FOR_NAME):
+    out_file = open(OUTPUT_FILE_FOR_NAME, 'r')
+    for line in out_file:
+        print(f"Your name is {line.title()}")
+    out_file.close()
+
+
+def question_3(OUTPUT_FILE_FOR_NUMBERS):
+    total = 0
+    out_file = open(OUTPUT_FILE_FOR_NUMBERS, 'r')
+    total += int(out_file.readline())
+    total += int(out_file.readline())
+    print(f"The total of the first 2 lines is {total}")
 
 
 main()
