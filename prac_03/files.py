627afc6de622d215312a9f26596e5288d5ec30da
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-09-30 22:36:16 +1000

    Added question 4 to files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,9 +1,10 @@
 def main():
     OUTPUT_FILE_FOR_NAME = "name.txt"
     OUTPUT_FILE_FOR_NUMBERS = "numbers.txt"
-    # question_1(OUTPUT_FILE_FOR_NAME)
-    # question_2(OUTPUT_FILE_FOR_NAME)
+    question_1(OUTPUT_FILE_FOR_NAME)
+    question_2(OUTPUT_FILE_FOR_NAME)
     question_3(OUTPUT_FILE_FOR_NUMBERS)
+    question_4(OUTPUT_FILE_FOR_NUMBERS)
 
 
 def question_1(OUTPUT_FILE_FOR_NAME):
@@ -25,7 +26,16 @@ def question_3(OUTPUT_FILE_FOR_NUMBERS):
     out_file = open(OUTPUT_FILE_FOR_NUMBERS, 'r')
     total += int(out_file.readline())
     total += int(out_file.readline())
+    out_file.close()
     print(f"The total of the first 2 lines is {total}")
+
+
+def question_4(OUTPUT_FILE_FOR_NUMBERS):
+    total = 0
+    out_file = open(OUTPUT_FILE_FOR_NUMBERS, 'r')
+    for line in out_file:
+        total += int(line)
+    print(f"the total of all lines is {total}")
 
 
 main()
