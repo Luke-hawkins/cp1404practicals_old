7a2d2bfb1e9babbdc9a04a1ca6a4f53d8c003710
Luke-hawkins <145887513+Luke-hawkins@users.noreply.github.com>
2023-10-08 15:29:01 +1000

    added formatted print statement as separate function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    summarize_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subjects_information = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -21,9 +22,18 @@ def get_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        print(parts)
+        # See if that worked
+        subjects_information.append(parts)
         print("----------")
     input_file.close()
+    return subjects_information
+
+
+def summarize_data(data):
+    """summarizes data for each subject"""
+    for subjects in data:
+        print(f"{subjects[0]} is taught by {subjects[1]} and has {subjects[2]} students")
 
 
 main()
